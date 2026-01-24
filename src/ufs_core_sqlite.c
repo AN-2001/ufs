89c1c9a9e3f0f7481040ef4c1601ca80059d01be
abd naran <abdnarane2001@gmail.com>
2026-01-24 22:10:23 +0200

    write_ufs_sqlite_in_memory: -- Implement ufsAddDirectory.

--- src/ufs_core_sqlite.c
+++ src/ufs_core_sqlite.c
@@ -13,14 +13,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define NUM_SQLITE_STATEMENTS ( 2 )
+
 typedef struct ufsSqliteStruct {
     sqlite3 *db;
-    sqlite3_stmt *statements[1];
+    sqlite3_stmt *statements[ NUM_SQLITE_STATEMENTS ];
 
 } ufsSqliteStruct;
 
-static const char *UFS_SCHEMA =
-    "CREATE TABLE IF NOT EXISTS ufsStorage(id INTEGER PRIMARY KEY, name STRING NOT NULL, isDir BOOLEAN);";
+static const char *UFS_SQL_TEXT[] = {
+
+    /* Schema command:                                                        */
+    "CREATE TABLE IF NOT EXISTS ufsStorage(id INTEGER PRIMARY KEY, name STRING NOT NULL, isDir BOOLEAN);",
+
+    /* Insert into the storage table:                                         */
+    "INSERT INTO ufsStorage (name, isDir) VALUES (?, ?);",
+
+    /* Query storage by name:                                                 */
+    "SELECT id from ufsStorage where name = ? and isDir = ?;",
+
+    NULL
+};
 
 static inline ufsSqliteStruct *prepareSqliteDb( sqlite3 *db );
 
@@ -28,11 +41,11 @@ static inline ufsSqliteStruct *prepareSqliteDb( sqlite3 *db );
 struct ufsSqliteStruct *prepareSqliteDb( sqlite3 *db )
 {
     char *errorMsg;
-    int res;
+    int res, i;
     ufsSqliteStruct *ufsSqlite;
 
     ufsSqlite = malloc( sizeof( *ufsSqlite ) );
-    if (!ufsSqlite) {
+    if ( !ufsSqlite ) {
         ufsErrno = UFS_OUT_OF_MEMORY;
         fprintf( stderr, "ufsSqlite: received error %s:"
                 "could not allocate memory for ufsSqliteStruct.\n", ufsStatusStrings[ ufsErrno ] );
@@ -40,13 +53,27 @@ struct ufsSqliteStruct *prepareSqliteDb( sqlite3 *db )
     }
 
     ufsSqlite -> db = db;
-    res = sqlite3_exec( db, UFS_SCHEMA, NULL, NULL, &errorMsg );
-    if (res != SQLITE_OK) {
+    res = sqlite3_exec( db, UFS_SQL_TEXT[ 0 ], NULL, NULL, &errorMsg );
+    if ( res != SQLITE_OK ) {
         ufsErrno = UFS_UNKNOWN_ERROR;
         fprintf( stderr, "ufsSqlite: received error %s:"
                 "%s.\n", ufsStatusStrings[ ufsErrno ], errorMsg );
         sqlite3_free( errorMsg );
         return NULL;
+    }
+
+    for ( i = 1; UFS_SQL_TEXT[ i ]; i++ ) {
+        res = sqlite3_prepare_v2( db,
+                                  UFS_SQL_TEXT[ i ],
+                                  -1,
+                                  &ufsSqlite -> statements[ i - 1 ],
+                                  NULL );
+        if (res != SQLITE_OK) {
+            ufsErrno = UFS_UNKNOWN_ERROR;
+            fprintf( stderr, "ufsSqlite: received error %s:"
+                    "%s.\n", ufsStatusStrings[ ufsErrno ], sqlite3_errmsg( db ) );
+            return NULL;
+        }
     }
 
     ufsErrno = UFS_NO_ERROR;
@@ -60,22 +87,21 @@ ufsType ufsInit()
     int res;
 
     res = sqlite3_open( ":memory:", &db );
-    if (!db) {
+    if ( !db ) {
         ufsErrno = UFS_OUT_OF_MEMORY;
         fprintf( stderr, "ufsSqlite: received error %s:"
                 "could not allocate memory for db connection.\n", ufsStatusStrings[ ufsErrno ] );
         return NULL;
     }
 
-    if (res != SQLITE_OK) {
+    if ( res != SQLITE_OK ) {
         ufsErrno = UFS_UNKNOWN_ERROR;
         fprintf( stderr, "ufsSqlite: received error %s:"
                 "%s.\n",ufsStatusStrings[ ufsErrno ],  sqlite3_errmsg( db ) );
         return NULL;
-
     }
     ret = prepareSqliteDb( db );
-    if (!ret)
+    if ( !ret )
         return NULL;
     ufsErrno = UFS_NO_ERROR;
     return ret;
@@ -83,13 +109,16 @@ ufsType ufsInit()
 
 void ufsDestroy( ufsType ufs )
 {
+    int i;
     ufsSqliteStruct *ufsSqlite;
-    if (!ufs) {
+    if ( !ufs ) {
         ufsErrno = UFS_NO_ERROR;
         return;
     }
 
     ufsSqlite = ufs;
+    for (i = 0; i < NUM_SQLITE_STATEMENTS; i++)
+        sqlite3_finalize( ufsSqlite -> statements[ i ] );
     sqlite3_close( ufsSqlite -> db );
     free( ufsSqlite );
     ufsErrno = UFS_NO_ERROR;
@@ -98,8 +127,46 @@ void ufsDestroy( ufsType ufs )
 ufsIdentifierType ufsAddDirectory( ufsType ufs,
                                    const char *name )
 {
+    ufsSqliteStruct *ufsSqlite;
+    int res;
+    if (!ufs || !name) {
+        ufsErrno = UFS_BAD_CALL;
+        return -1;
+    }
+
+    ufsSqlite = ufs;
+
+    sqlite3_bind_text( ufsSqlite -> statements[ 1 ], 1, name, -1, SQLITE_TRANSIENT );
+    sqlite3_bind_int( ufsSqlite -> statements[ 1 ], 2, 1);
+
+    res = sqlite3_step( ufsSqlite -> statements[ 1 ] );
+    if ( res == SQLITE_ROW ) {
+        ufsErrno = UFS_ALREADY_EXISTS;
+        fprintf( stderr, "ufsSqlite: received error %s:"
+                "Directory already exists.\n", ufsStatusStrings[ ufsErrno ] );
+        return -1;
+    }
+
+    if ( res != SQLITE_DONE ) {
+        ufsErrno = UFS_UNKNOWN_ERROR;
+        fprintf( stderr, "ufsSqlite: received error %s:"
+                "%s.\n",ufsStatusStrings[ ufsErrno ], sqlite3_errmsg( ufsSqlite -> db ) );
+        return -1;
+    }
+
+    sqlite3_bind_text( ufsSqlite -> statements[ 0 ], 1, name, -1, SQLITE_TRANSIENT );
+    sqlite3_bind_int( ufsSqlite -> statements[ 0 ], 2, 1);
+
+    res = sqlite3_step( ufsSqlite -> statements[ 0 ] );
+    if ( res != SQLITE_DONE ) {
+        ufsErrno = UFS_UNKNOWN_ERROR;
+        fprintf( stderr, "ufsSqlite: received error %s:"
+                "%s.\n",ufsStatusStrings[ ufsErrno ], sqlite3_errmsg( ufsSqlite -> db ) );
+        return -1;
+    }
+
     ufsErrno = UFS_NO_ERROR;
-    return 0;
+    return sqlite3_last_insert_rowid( ufsSqlite -> db );
 }
 
 ufsIdentifierType ufsAddFile( ufsType ufs,
