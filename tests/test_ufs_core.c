6f7ca1053fb3110f2d354ed9cb3fd3bbb9b1a8f0
abd naran <abdnarane2001@gmail.com>
2026-01-31 21:03:52 +0200

    write_ufs_test_suite: -- Fix minor problem in test_ufs_remove_mapping_remove_then_probe.

--- tests/test_ufs_core.c
+++ tests/test_ufs_core.c
@@ -1412,7 +1412,7 @@ static void test_ufs_remove_mapping_remove_then_probe( void **state )
     status = ufsRemoveMapping( ufsStruct -> ufs, areaId, dirId);
     ASSERT_UFS_STATUS_NO_ERROR( status );
 
-    status = ufsProbeMapping( ufsStruct -> ufs, dirId, areaId );
+    status = ufsProbeMapping( ufsStruct -> ufs, areaId, dirId );
     ASSERT_UFS_STATUS( status, UFS_MAPPING_DOES_NOT_EXIST );
 
 }
